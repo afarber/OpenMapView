23a20d9a1a57c6eb5a34a40a7f6a67fc822571e3
Alexander Farber <farber72@outlook.de>
2025-11-08 21:33:04 +0100

    Display glow effect only while tiles are downloaded

--- openmapview/src/main/kotlin/de/afarber/openmapview/OpenMapView.kt
+++ openmapview/src/main/kotlin/de/afarber/openmapview/OpenMapView.kt
@@ -105,7 +105,6 @@ class OpenMapView
             controller.setOnTileLoadedCallback {
                 invalidate()
             }
-            attributionOverlay.isGlowEnabled = uiSettings.isAttributionGlowEnabled
         }
 
         override fun dispatchDraw(canvas: Canvas) {
@@ -114,7 +113,7 @@ class OpenMapView
             if (uiSettings.isZoomControlsEnabled) {
                 zoomControlsOverlay.draw(canvas, width, height)
             }
-            attributionOverlay.isGlowEnabled = uiSettings.isAttributionGlowEnabled
+            attributionOverlay.isGlowEnabled = uiSettings.isAttributionGlowEnabled && controller.isDownloadingTiles()
             attributionOverlay.draw(canvas, width, height)
         }
 
